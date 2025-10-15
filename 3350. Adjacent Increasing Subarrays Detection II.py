ec5bf76ecb2690a7344efeab8c013692b18dac64
Arnab Chakraborty <chakrabortyarnab27@gmail.com>
2025-10-16 00:48:44 +0530

    15.10.2025

---
+++ 3350. Adjacent Increasing Subarrays Detection II.py
@@ -0,0 +1,16 @@
+class Solution:
+  def maxIncreasingSubarrays(self, nums: list[int]) -> int:
+    ans = 0
+    increasing = 1
+    prevIncreasing = 0
+
+    for a, b in itertools.pairwise(nums):
+      if b > a:
+        increasing += 1
+      else:
+        prevIncreasing = increasing
+        increasing = 1
+      ans = max(ans, increasing // 2)
+      ans = max(ans, min(prevIncreasing, increasing))
+
+    return ans
